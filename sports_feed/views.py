837e8c0cf2acad4032dafc341276b8bf65bac646
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2024-01-18 17:48:28 -0500

    creating the connect with the api

--- sports_feed/views.py
+++ sports_feed/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
+from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.static import players
+from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats import endpoints
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    data_pontos = endpoints.leagueleaders.LeagueLeaders(
+        season=SeasonAll.current_season, per_mode48='PerGame')
+    df_pontos = data_pontos.league_leaders.get_data_frame()
+    jogador = (df_pontos.PLAYER[2])
+
+    print('JOGADOR COM MAIOR MEDIA DE PONTOS: ', jogador)
+
+    return render(request, 'index.html', {jogador: 'jogador'})
