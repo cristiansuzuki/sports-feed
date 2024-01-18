50fe4db48b2712c764c64938f9b56aa7a981eccc
Cristian Suzuki <cristiansuzuki5@hotmail.com>
2024-01-18 17:27:42 -0500

    creating blank template

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('sports_feed.urls')),
     path('admin/', admin.site.urls),
 ]
