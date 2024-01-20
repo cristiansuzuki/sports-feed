dbdaa42a4aec49a49a56f7993a766bbd8116a28f
cristiansuzuki <cristiansuzuki5@hotmail.com>
2024-01-20 18:23:51 -0500

    creating assist view

--- sports_feed/views.py
+++ sports_feed/views.py
@@ -23,6 +23,7 @@ def index(request):
     jogador4 = (df_pontos.PLAYER[4], df_pontos.PTS[4])
     # nesse bloco acima, armazenei as variáveis relacionadas as suas respectivas posições dos jogadores
 
+    # esse bloco abaixo segue a mesma ideia, so que para rebotes ao inves de pontos
     data_rebotes = endpoints.leagueleaders.LeagueLeaders(season=SeasonAll.current_season, stat_category_abbreviation='REB', per_mode48='PerGame')
     df_rebotes = data_rebotes.league_leaders.get_data_frame()
 
@@ -32,5 +33,17 @@ def index(request):
     jogador_reb3 = (df_rebotes.PLAYER[3], df_rebotes.REB[3])
     jogador_reb4 = (df_rebotes.PLAYER[4], df_rebotes.REB[4])
 
+    # assistencias
+    data_assistencias = endpoints.leagueleaders.LeagueLeaders(season=SeasonAll.current_season, stat_category_abbreviation='AST', per_mode48='PerGame')
+    df_assistencias = data_assistencias.league_leaders.get_data_frame()
+
+    jogador_assist = (df_assistencias.PLAYER[0], df_assistencias.AST[0])
+    jogador_assist1 = (df_assistencias.PLAYER[1], df_assistencias.AST[1])
+    jogador_assist2 = (df_assistencias.PLAYER[2], df_assistencias.AST[2])
+    jogador_assist3 = (df_assistencias.PLAYER[3], df_assistencias.AST[3])
+    jogador_assist4 = (df_assistencias.PLAYER[4], df_assistencias.AST[4])
+
     return render(request, 'index.html', {'jogador': jogador, 'jogador1': jogador1,'jogador2': jogador2,'jogador3': jogador3,'jogador4': jogador4,
-    'jogador_reb':jogador_reb, 'jogador_reb1':jogador_reb1, 'jogador_reb2':jogador_reb2, 'jogador_reb3':jogador_reb3, 'jogador_reb4':jogador_reb4, })
+    'jogador_reb':jogador_reb, 'jogador_reb1':jogador_reb1, 'jogador_reb2':jogador_reb2, 'jogador_reb3':jogador_reb3, 'jogador_reb4':jogador_reb4, 
+    'jogador_assist': jogador_assist, 'jogador_assist1': jogador_assist1, 'jogador_assist2': jogador_assist2, 'jogador_assist3': jogador_assist3, 'jogador_assist4': jogador_assist4, 
+    })
